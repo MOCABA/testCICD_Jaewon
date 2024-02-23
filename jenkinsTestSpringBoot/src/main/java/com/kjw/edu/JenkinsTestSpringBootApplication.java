5b75b205c9c5475a01703ba5e2371e28eb746b75
kjwkjw02160 <kjwkjw02160@naver.com>
2024-02-23 09:37:42 +0900

    TestSpringBoot

---
+++ jenkinsTestSpringBoot/src/main/java/com/kjw/edu/JenkinsTestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kjw.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestSpringBootApplication.class, args);
+	}
+
+}
---
+++ jenkinsTestSpringBoot/src/test/java/com/kjw/edu/JenkinsTestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjw.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
